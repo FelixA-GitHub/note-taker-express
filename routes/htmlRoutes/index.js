861ec35d71e7bd18237d6e9ca9d6d4a9f91c7756
Your Name <felix.acevedo.tech@gmail.com>
2022-04-16 11:07:21 -0400

    reworking routes files

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -3,13 +3,17 @@ const router = require('express').Router();
 
 //this route takes us to notes specifically in the front end browser
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/notes.html'));
-  });
+  res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
 
 //this accounts for a client making a request for a route that does not exist
 //"*" acts as a wildcard, meaning any request not previously defined will receive the homepage as the response
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
-  });
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
   
 module.exports = router;
